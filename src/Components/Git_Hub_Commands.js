4b1305fdcf1c7f04baddd768b399161c0f3c1011
Sivaji777 <86228129+Sivaji777@users.noreply.github.com>
2023-01-02 19:05:53 +0530

    Update Git_Hub_Commands.js

--- src/Components/Git_Hub_Commands.js
+++ src/Components/Git_Hub_Commands.js
@@ -25,6 +25,13 @@ function Git_Hub_Commands() {
         2) git commit -m "Dummy Data" 
         3) git push
       */}
+ 
+      <h1>how to take pull from main branch code to your local branch</h1>
+      {/* 
+        Steps:---
+         1) git checkout dmgr2
+         2) git pull origin master
+      */}
 
       <h1>How to add add new origin URLS </h1>
       {/* 
